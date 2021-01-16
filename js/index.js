2d2e16f2f266ee1f5c5a557ef95a6b8efadd7abc
Nicolas Froidure <nicolas.froidure@gmail.com>
2021-01-16 12:09:05 +0100

    Build 2021-01-16T11:09:05.721Z

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'; // The following module is dynamically generated
+// TODO: Cleanup that shit
+
+var searchIndex = require('./search.index'); // eslint-disable-line
+
+
+module.exports = searchIndex;
